46d5831dd68f9adfef11fd525f037f7995538150
Nielsington <declercqniels@hotmail.com>
2023-03-27 17:06:27 +0200

    Add Tailwind CDN and styling

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
-<body>
-    <p>Hello World!</p>
+<body class="bg-emerald-700">
+    <h1 class="text-center text-[2.5rem]">This is Blade!</h1>
     <ul>
         <li><a href="{{route('contact')}}">Contact page</a></li>
         <li><a href="{{route('product')}}">Product page</a></li>
