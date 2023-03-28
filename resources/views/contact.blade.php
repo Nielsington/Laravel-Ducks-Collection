263d665d6303074715b3573a77a45f0cfbd9277b
Nielsington <declercqniels@hotmail.com>
2023-03-28 18:48:51 +0200

    Add sections to view
    Add form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,17 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>CONTACT PAGE</h1>
-    <ul>
-        <li><a href="{{route('home')}}">Homepage</a></li>
-        <li><a href="{{route('product')}}">Product page</a></li>
-        <li><a href="{{route('about')}}">About page</a></li>
+@extends('layout')
+@section('header')
+<header>
+    <h4 class="text-center text-[2.5rem]">CONTACT PAGE</h4>
+</header>
+@endsection
+
+@section('nav')
+<nav>
+    <ul class="list-disc ml-8">
+        <li><a class="text-zinc-50" href="{{route('home')}}">Homepage</a></li>
+        <li><a class="text-zinc-50" href="{{route('product')}}">Product page</a></li>
+        <li><a class="text-zinc-50" href="{{route('about')}}">About page</a></li>
     </ul>
-</body>
-</html>
+</nav>
+@endsection
+
+@section('form')
+<form action="{{route('handleForm')}}" method="POST" class="flex flex-col w-[20%] m-auto">
+@csrf
+@if ($errors->any())
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+    <label class="text-zinc-50" for="name">Name:</label>
+    <input type="text" id="name" name="name">
+    <label class="text-zinc-50" for="firstName">First name:</label>
+    <input type="text" id="firstName" name="firstName">
+    <label class="text-zinc-50" for="subject">Subject:</label>
+    <input type="text" id="subject" name="subject">
+    <label class="text-zinc-50" for="email">Email:</label>
+    <input type="email" id="email" name="email">
+    <label class="text-zinc-50" for="description">Describe your problem:</label>
+    <textarea id="description" rows="4" cols="50" name="description"></textarea>
+    <button class="mt-[2rem] border-2 border-yellow-800 bg-white rounded-full" type="submit">Submit ticket!</button>
+</form>
+@endsection
