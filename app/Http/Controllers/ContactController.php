d80db5252ab4c904e74b4634a8e6f81306f5adea
Nielsington <declercqniels@hotmail.com>
2023-03-28 18:51:45 +0200

    Add form validation

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,8 +6,26 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    public function show()
+    public function show(Request $request)
     {
+        if ($request->isMethod('post')) {
+            $name = $this->getForm($request);
+            return view('contact',['name' => $name]);
+        }
+        
         return view('contact');
     }
+
+    private function getForm()
+    {
+        $formData = request()->validate([
+                            'name' => ['required', 'max:255'],
+                            'firstName' => ['required', 'max:255'],
+                            'subject' => ['required', 'max:255'],
+                            'email' => ['required', 'email', 'max:255'],
+                            'description' => ['required', 'min:10', 'max:1500'],
+                        ]);
+            dd('Validation passed!');
+            return $formData['name'];
+}
 }
