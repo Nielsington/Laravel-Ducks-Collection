603eb017040d39b1c63379772e032956b5042f16
Nielsington <declercqniels@hotmail.com>
2023-03-30 14:20:02 +0200

    Add custom validation error messages

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -23,9 +23,16 @@ class ContactController extends Controller
                             'firstName' => ['required', 'max:255'],
                             'subject' => ['required', 'max:255'],
                             'email' => ['required', 'email', 'max:255'],
-                            'description' => ['required', 'min:10', 'max:1500'],
-                        ]);
-            dd('Validation passed!');
+                            'description' => ['required', 'min:10', 'max:1500']
+                            ],
+                            [   
+                                'name.required' => 'Please enter your name',    
+                                'name.max' => 'Your name should not be more than 255 characters',    
+                                'email.required' => 'Please enter your email address',    
+                                'email.email' => 'Please enter a valid email address',    
+                                'email.max' => 'Your email should not be more than 255 characters',    
+                                'description.min' => 'Your description should not be less than 10 characters'
+                            ]);
             return $formData['name'];
 }
 }
