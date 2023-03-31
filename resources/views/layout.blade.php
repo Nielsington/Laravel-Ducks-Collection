1c80fc504e71ddfc7995f32d25e949c12263e34c
Nielsington <declercqniels@hotmail.com>
2023-03-31 17:53:28 +0200

    Add navigation component
    Add about section
    Add link to SCSS & Tailwind

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
     <title>LaraBlade</title>
+    @vite('resources/css/app.scss')
 </head>
 
-<body class="bg-emerald-700 flex flex-col min-h-screen"> 
-
+<body class="bg-cyan-900 flex flex-col min-h-screen"> 
+    
     @yield('header')
-    @yield('nav')
-
+    <x-navigation />
     <main class="flex-grow">
         @yield('form')
+        @yield('about')
     </main>
     
     <footer class="bg-slate-500	mt-auto text-center">
