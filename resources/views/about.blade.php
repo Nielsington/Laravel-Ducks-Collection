a087cda3d6caaf6fe1337617e230787a56dc1ff4
Nielsington <declercqniels@hotmail.com>
2023-03-31 17:55:31 +0200

    Add 'About us' content to about section
    Remove navigation
    Adjust header CSS

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,16 +1,28 @@
 @extends('layout')
 @section('header')
 <header>
-    <h4 class="text-center text-[2.5rem]">ABOUT PAGE</h4>
+    <h4 class="text-center text-[3rem]">ABOUT US</h4>
 </header>
 @endsection
 
-@section('nav')
-<nav>
-    <ul class="list-disc ml-8">
-        <li><a class="text-zinc-50" href="{{route('home')}}">Homepage</a></li>
-        <li><a class="text-zinc-50" href="{{route('contact')}}">Contact page</a></li>
-        <li><a class="text-zinc-50" href="{{route('product')}}">Product page</a></li>
-    </ul>
-</nav>
+@section('about')
+    <div class="w-[50%] m-auto text-zinc-50 ">
+        <div>
+            <p class="text-[1.2rem]">
+                Welcome to LaraBlade!<br><br>
+
+                LaraBlade is a small project that was created for testing and educational purposes. The main goal of this project was to learn Laravel, a popular PHP framework used for web development. <br><br>
+
+                LaraBlade was created solely for educational purposes, and all the content you see on this website is for testing purposes only. This project is not intended for commercial use or for the production of any products or services. <br><br>
+
+                We hope that by exploring LaraBlade, you will gain a better understanding of Laravel and its capabilities. Whether you are a beginner or an experienced developer, we believe that there is something for everyone to learn here. <br><br>
+
+                Please note that while we have made every effort to ensure the accuracy and reliability of the information presented on this website, we cannot guarantee that everything is 100% accurate or up-to-date. If you have any questions or concerns about the content on this website, please feel free to contact us. <br><br>
+
+                Thank you for visiting LaraBlade, and we hope you enjoy your stay! <br><br>
+
+                --This text was generated with ChatGPT-- 
+            </p>
+        </div>
+    </div>
 @endsection
