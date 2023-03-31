5af4412138670c824eef0262f874ecd5c8d53b92
Nielsington <declercqniels@hotmail.com>
2023-03-31 17:49:49 +0200

    Add Hot Module Replacement
    Add Tailwind & SCSS

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,39 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith('.blade.php')) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    });
+                }
+            },
+        }
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
+                    @import 'tailwindcss/base';
+                    @import 'tailwindcss/components';
+                    @import 'tailwindcss/utilities';
+                `,
+            },
+        },
+    },
 });
