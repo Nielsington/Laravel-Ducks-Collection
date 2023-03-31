5eb272b46e3bbc22c525f44e2afb9018615e13c3
Nielsington <declercqniels@hotmail.com>
2023-03-31 17:50:30 +0200

    Add navigation component

---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,8 @@
+<nav class="mt-[2rem] mb-[3rem]">
+    <ul class="flex justify-evenly">
+        <a class="text-zinc-50" href="{{route('home')}}">Homepage</a>
+        <a class="text-zinc-50" href="{{route('contact')}}">Contact page</a>
+        <a class="text-zinc-50" href="{{route('product')}}">Product page</a>
+        <a class="text-zinc-50" href="{{route('about')}}">About page</a>
+    </ul>
+</nav>
