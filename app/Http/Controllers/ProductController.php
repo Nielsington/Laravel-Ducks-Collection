39ff0ae6c10b0597e1e910161e0de9c2cbf14002
Nielsington <declercqniels@hotmail.com>
2023-04-11 18:46:08 +0200

    Add functions to fetch product(s) from DB

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Duck;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Collection;
 
 class ProductController extends Controller
 {
     public function show()
     {
-        return view('product');
+        $all_ducks = $this->getAllProducts();
+        return view('productOverview', ['all_ducks' => $all_ducks]);
+    }
+
+    private function getAllProducts(): Collection
+    {
+        $all_ducks = Duck::all();
+        return $all_ducks;
+    }
+
+    public function showProduct(int $id)
+    {
+        $product = $this->getProduct($id);
+        clock()->info($product);
+        //TODO: use $product data
+        return view('product', ['product' => $product]);
+    }
+
+    private function getProduct(int $id): Duck
+    {
+        $product = Duck::find($id);
+        return $product;
     }
 }
