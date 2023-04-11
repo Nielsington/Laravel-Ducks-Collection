cc48c5032d786a93623b0c881c288171898983d2
Nielsington <declercqniels@hotmail.com>
2023-04-11 13:25:59 +0200

    Remove header section

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,6 +1,5 @@
 @extends('layout')
-@section('header')
+
 <header>
     <h4 class="text-center text-[3rem]">HOME PAGE</h4>
 </header>
-@endsection
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,6 +1,5 @@
 @extends('layout')
-@section('header')
+
 <header>
     <h4 class="text-center text-[3rem]">PRODUCT PAGE</h4>
 </header>
-@endsection
