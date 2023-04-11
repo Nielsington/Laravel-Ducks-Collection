fc0b09a16e91214c8e2867e001f4df235c2d2c98
Nielsington <declercqniels@hotmail.com>
2023-04-11 18:41:20 +0200

    Add single product page

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,3 +1,13 @@
 @extends('layout')
 <h4 class="text-center text-[3rem]">PRODUCT PAGE</h4>
 <x-navigation />
+
+@section('content')
+<div class="w-[70%] m-auto">
+    <h6 class="text-[1.2rem] text-[#CFE031] font-bold text-center">{{$product->Theme}}</h6>
+    <p class="text-[#FFFFFF]">Price: {{$product->Price}}</p>
+    <p class="text-[#FFFFFF]">Rarity: {{$product->Rarity}}</p>
+    <p class="text-[#FFFFFF]">Color: {{$product->Color}}</p>
+    <p class="text-[#FFFFFF]">Manufacturer: {{$product->Manufacturer}}</p>
+</div>
+@endsection
