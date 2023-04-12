041ede385edd1e5298d905fc6d9796b63ac474e4
Nielsington <declercqniels@hotmail.com>
2023-04-12 12:48:23 +0200

    Improve Route Model Binding

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/', [HomepageController::class, 'show'])->name("home");
 Route::get('/about', [AboutController::class, 'show'])->name("about");
 
 Route::get('/product', [ProductController::class, 'show'])->name("product");
-Route::get('/product/{id}', [ProductController::class, 'showProduct']);
+Route::get('/product/{duck}', [ProductController::class, 'showProduct']);
 
 Route::get('/contact', [ContactController::class, 'show'])->name("contact");
 Route::post('/contact', [ContactController::class, 'getForm'])->name("submit-contact");
